9c4b8052fcc72ac212b07cf9bc42997980db281e
JonMortem <54084979+JonMortem@users.noreply.github.com>
2019-10-10 17:13:12 -0500

    Update main.c

--- TAREAS/1/main.c
+++ TAREAS/1/main.c
@@ -1,13 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
-//programa para hacer echo de una oracion
 int main(int argc, char *argu[]){
-int m;
 char *palabra;
-for (int i=1; i<argc; i++){
-	palabra=argu[i];
+	palabra=argu[1];
 	printf("%s ",palabra);
-}
-printf("\n");
 return 0;
 }
