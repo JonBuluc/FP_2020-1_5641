98ea3908fe91ca752a1c9f81b8b5281a187256c0
JonMortem <jonanb515@gmail.com>
2019-10-17 19:05:05 -0500

    tarea 3

---
+++ TAREAS/3/main.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(int argc,char* argu[]){
+	float num1,num2,suma,resta,multiplicacion,division;
+	num1=atof(argu[1]);
+	num2=atof(argu[2]);
+	suma=num1+num2;
+	resta=num1-num2;
+	multiplicacion=num1*num2;
+	division=num1/num2;
+	printf("%.2f\n",suma);
+	printf("%.2f\n",resta);
+	printf("%.2f\n",multiplicacion);
+	printf("%.2f\n",division);
+}
