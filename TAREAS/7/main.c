763e32e094f4115fc88405267ce8220489226d8d
Jon <jonanb515@gmail.com>
2019-11-04 17:37:09 -0600

    tarea 7

---
+++ TAREAS/7/main.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(int arcgc, char*argu[]){
+	float num1,num2,resultado=1;
+	num1=atof(argu[1]);
+	num2=atof(argu[2]);
+	for(int i=0;i<num2;i++){
+		resultado=resultado*num1;
+	}
+	printf("%.2f\n",resultado);
+	return 0;
+}	
