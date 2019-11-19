a209630103c5407a362a65e928712fc6673336bf
Jon <jonanb515@gmail.com>
2019-11-19 17:36:33 -0600

    agregue los comentarios a la tarea 7

--- TAREAS/7/main.c
+++ TAREAS/7/main.c
@@ -1,12 +1,17 @@
 #include<stdio.h>
 #include<stdlib.h>
 int main(int arcgc, char*argu[]){
+	//resultado empieza con 1 porque es el neutro multipllicativo
 	float num1,num2,resultado=1;
+	//el numero uno sera la base
 	num1=atof(argu[1]);
+	//y el numero dos sera el exponente
 	num2=atof(argu[2]);
+	//se utiliza un for para multiplicar la base las veces que diga el exponente
 	for(int i=0;i<num2;i++){
 		resultado=resultado*num1;
 	}
+	//se imprime el resultado
 	printf("%.2f\n",resultado);
 	return 0;
 }	
